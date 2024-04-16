db2b19edc7ffe388abf009d1faad8ce504326e2c
Ashishhm04 <167194667+Ashishhm04@users.noreply.github.com>
2024-04-16 20:16:16 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -26,10 +26,13 @@ locals {
   subnet_cidr_private = cidrsubnet(var.vpc_cidr, 4, 1)
 }
 
+data "aws_availability_zones" "available" {
+  state = "available"
+}
 
 
 #Internet gateway for public subnet
-resource "aws_internet_gateway" "ig" {
+resource "aws_internet_gateway" "eg_prod_bastion_label_ig" {
   vpc_id = aws_vpc.main.id
   tags = {      
     Name        = "module.label_vpc.tags"
@@ -37,17 +40,17 @@ resource "aws_internet_gateway" "ig" {
 }
 
 # Elastic IP for NAT 
-resource "aws_eip" "nat_eip" {
+resource "aws_eip" "eg_prod_bastion_label_nat_eip" {
   vpc        = true
-  depends_on = [aws_internet_gateway.ig]
+  depends_on = [aws_internet_gateway.eg_prod_bastion_label_ig]
   tags       = module.label_vpc.tags
 }
 
 #NAT
-resource "aws_nat_gateway" "nat" {
-  allocation_id = "${aws_eip.nat_eip.id}"
+resource "aws_nat_gateway" "eg_prod_bastion_label_ig_nat" {
+  allocation_id = "${aws_eip.eg_prod_bastion_label_nat_eip.id}"
   subnet_id     = "${aws_subnet.public_subnet.id}"  
-  depends_on    = [aws_internet_gateway.ig]
+  depends_on    = [aws_internet_gateway.eg_prod_bastion_label_ig]
   tags = {
     Name        = "module.label_vpc.tags"
   }
@@ -57,7 +60,7 @@ resource "aws_nat_gateway" "nat" {
 resource "aws_subnet" "public_subnet" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "${local.subnet_cidr_public}"
-  availability_zone       = "${var.aws_availability_zones}"
+  availability_zone       = data.aws_availability_zones.available.names[0]
   map_public_ip_on_launch = true
   tags = {
     Name        = "module.label_vpc.tags"
@@ -68,7 +71,7 @@ resource "aws_subnet" "public_subnet" {
 resource "aws_subnet" "private_subnet" {
   vpc_id                  = "${aws_vpc.main.id}"
   cidr_block              = "${local.subnet_cidr_private}"
-  availability_zone       = "${var.aws_availability_zones}"
+  availability_zone       = data.aws_availability_zones.available.names[0]
   map_public_ip_on_launch = false
   tags = {
     Name        = "module.label_vpc.tags"
@@ -76,7 +79,7 @@ resource "aws_subnet" "private_subnet" {
 }
 
 # Routing table for private subnet 
-resource "aws_route_table" "private" {
+resource "aws_route_table" "eg_prod_bastion_label_private" {
   vpc_id = "${aws_vpc.main.id}"
   tags = {
     Name        = "module.label_vpc.tags"
@@ -84,38 +87,38 @@ resource "aws_route_table" "private" {
 }
 
 # Routing table for public subnet 
-resource "aws_route_table" "public" {
+resource "aws_route_table" "eg_prod_bastion_label_public" {
   vpc_id = "${aws_vpc.main.id}"
   tags = {
     Name        =  "module.label_vpc.tags"
   }
 }
 
-resource "aws_route" "public_internet_gateway" {
-  route_table_id         = "${aws_route_table.public.id}"
+resource "aws_route" "eg_prod_bastion_label_public_internet_gateway" {
+  route_table_id         = "${aws_route_table.eg_prod_bastion_label_public.id}"
   destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = "${aws_internet_gateway.ig.id}"
+  gateway_id             = "${aws_internet_gateway.eg_prod_bastion_label_ig.id}"
 }
 
 resource "aws_route" "private_nat_gateway" {
-  route_table_id         = "${aws_route_table.private.id}"
+  route_table_id         = "${aws_route_table.eg_prod_bastion_label_private.id}"
   destination_cidr_block = "0.0.0.0/0"
-  nat_gateway_id         = "${aws_nat_gateway.nat.id}"
+  nat_gateway_id         = "${aws_nat_gateway.eg_prod_bastion_label_ig_nat.id}"
 }
 
 # Route table associations 
 resource "aws_route_table_association" "public" {
   subnet_id      = "${aws_subnet.public_subnet.id}"
-  route_table_id = "${aws_route_table.public.id}"
+  route_table_id = "${aws_route_table.eg_prod_bastion_label_public.id}"
 }
 
 resource "aws_route_table_association" "private" {
   subnet_id      = "${aws_subnet.private_subnet.id}"
-  route_table_id = "${aws_route_table.private.id}"
+  route_table_id = "${aws_route_table.eg_prod_bastion_label_private.id}"
 }
 
 # VPC's Default Security Group 
-resource "aws_security_group" "default" {
+resource "aws_security_group" "eg_prod_bastion_label_security_group" {
   name        = "default-sg"
   description = "Default security group to allow inbound/outbound from the VPC"
   vpc_id      = "${aws_vpc.main.id}"
